f421108f6e2273c7ffe165470a344540ae81eaa1
xbedna70 <xbedna70@vutbr.cz>
2020-10-07 17:26:44 +0200

    final cv1

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -17,12 +17,28 @@
  ******************************************************************************
  */
 
+//morseovka
+
+#include "stm32f0xx.h"
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;		// zapnuti citani
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;	//	nastaveni jako OUT
+
+
+	for(;;){
+		uint32_t pole = 0b10101001110111011100101010000000; //binarne SOS
+		for (uint8_t i = 0; i < 32; i++) {					//cyklus pro nacteni bitu
+			if(pole & 0x80000000) GPIOA->BSRR = (1<<5);		//testuje se zda bit z morseovky je roven 1 pokud ano, tak se nastavi LED
+			else GPIOA->BRR = (1<<5);
+			pole <<=1;
+
+			for (volatile uint32_t i = 0; i < 100000; i++) {}	//cekani
+			}
+		}
 }
